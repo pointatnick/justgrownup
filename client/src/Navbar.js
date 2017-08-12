0b35f4ef0e0145e7eb6fb8644a0017b9f5838e1f
pointatnick <nicholashma@gmail.com>
2017-08-12 15:03:40 -0700

    express and react working

---
+++ client/src/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './css/Navbar.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <h1 className="logo">Just Grown Up</h1>
+        <ul>
+          <li><a href="#recent">Recent Posts</a></li>
+          <li><a href="#articles">Articles</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact Us</a></li>
+          <li><input type="text" placeholder="Search" /></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
---
+++ client/src/PostList.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Post from './Post';
+import PostAdd from './PostAdd';
+
+class PostList extends Component {
+  constructor() {
+    super();
+    this.state = { data: [] }
+    this.createPost = this.createPost.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  createPost(newPost) {
+    const posts = this.state.data.slice();
+    posts.push(newPost);
+    this.setState({ data: posts });
+  }
+
+  loadData() {
+    fetch('/api/posts').then(res => res.json())
+    .then(posts => {
+      this.setState({ data: posts });
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  render() {
+    const postNodes = this.state.data.map(post =>
+      <Post author={post.author} title={post.title} body={post.body} />
+    );
+    return (
+      <div>
+        <h1>Posts</h1>
+        <PostAdd createPost={this.createPost} />
+        <div className="postlist-container">
+          {postNodes}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PostList;

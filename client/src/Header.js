659a2302b4966f1c4fff9fb6d6633375a83bbc7e
pointatnick <nicholashma@gmail.com>
2017-09-13 07:18:05 -0700

    add logo

--- client/src/Header.js
+++ client/src/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
   render() {
     return (
       <header>
-        <h1 className="logo">Just Grown Up</h1>
+        <img id="logo-header" src="/assets/logo.png" />
         <nav>
           <ul>
             <li><Link to="/">Home</Link></li>

beb03d408edec594e7c212aef8594e7167db1bb1
pointatnick <nicholashma@gmail.com>
2017-11-12 18:16:57 -0800

    cleaner navbar

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import './Header.css';
+// import './Header.css';
 
 export default class Header extends Component {
   render() {
     return (
       <header>
-        <img id="logo-header" src="/images/logo.png" alt=""/>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/articles">Articles</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact Us</Link></li>
-            <li><input type="text" placeholder="Search" /></li>
-          </ul>
+        <nav className="navbar has-shadow">
+          <div className="navbar-brand">
+            <img className="navbar-item" id="logo-header" src="/images/logo.png" alt=""/>
+          </div>
+          <div className="navbar-menu">
+            <div className="navbar-end">
+              <Link to="/" className="navbar-item">Home</Link>
+              <Link to="/articles" className="navbar-item">Articles</Link>
+              <Link to="/about" className="navbar-item">About</Link>
+              <Link to="/contact" className="navbar-item">Contact Us</Link>
+              <div className="navbar-item field">
+                <p className="control has-icons-left">
+                  <input className="input" type="text" placeholder="Search"/>
+                  <span className="icon is-small is-left">
+                    <i className="fa fa-search"></i>
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
         </nav>
       </header>
     );
--- client/src/Preview.js
+++ client/src/Preview.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Preview extends Component {
   render() {
     return (
-      <div style={{width: 300, height: 700}} className="card">
+      <div style={{width: 300, height: 500}} className="card">
         <div className="card-image">
           <figure className="image is-square">
             <img src="http://i0.kym-cdn.com/photos/images/original/001/250/216/305.jpg"/>
